31493d92486f317d468aa8868c8ba022002f4d79
Tim Grupczynski <t.grupczynski@googlemail.com>
2020-07-07 16:17:08 +0200

    Added BreadthFirstSearch

--- src/de/vitbund/vitmaze/players/Pathfinder.java
+++ src/de/vitbund/vitmaze/players/Pathfinder.java
@@ -1,6 +1,7 @@
 package de.vitbund.vitmaze.players;
 
 import java.util.*;
+import java.util.Map.Entry;
 
 import de.vitbund.vitmaze.players.InputHandler.InputType;
 import de.vitbund.vitmaze.players.Vector2.Direction;
@@ -39,29 +40,87 @@ public class Pathfinder {
 	}
 
 	public void move() {
-		boolean isStartPos = player.getPosition().equals(player.getStartPos());
-		System.err.println("Player is startpos: " + isStartPos);
-		if (!isStartPos && wayToPoint.isEmpty()) {
-			player.move(Player.revertDirection(world.worldMap.get(player.getPosition()).exploredByLooking));
-		} else {
-			if(wayToPoint.isEmpty()) {
-				wayToPoint = new ArrayList<Waypoint>();
-				Waypoint waypoint = waypointQ.poll();
-				if (waypoint != null) {
-					wayToPoint.add(waypoint);
-					while(!waypoint.explorerPos.equals(player.getStartPos())) {
-						waypoint = world.worldMap.get(waypoint.explorerPos);
-						wayToPoint.add(waypoint);
-					}
-					Direction moveDir = wayToPoint.get(wayToPoint.size() - 1).exploredByLooking;
-					wayToPoint.remove(wayToPoint.size() - 1);
-					player.move(moveDir);
-				}
-			} else {
-				Direction moveDir = wayToPoint.get(wayToPoint.size() - 1).exploredByLooking;
-				wayToPoint.remove(wayToPoint.size() - 1);
-				player.move(moveDir);
-			}
+		if(wayToPoint.isEmpty()) {
+			findPath(player.getPosition(), waypointQ.poll().position);
 		}
+		for (Waypoint waypoint : wayToPoint) {
+			System.err.println(waypoint.exploredByLooking);
+		}
+		Direction moveDirection = wayToPoint.get(0).exploredByLooking;
+		wayToPoint.remove(0);
+		player.move(moveDirection);
+	}
+
+	public void findPath(Vector2 startPos, Vector2 targetPos) {
+		//Erstelle exakte Kopie der World Map
+		Map<Vector2, Waypoint> map = new HashMap<Vector2, Waypoint>();
+		map.putAll(world.worldMap);
+		
+		//Setze Attribute der Kopie zurück
+		for (Entry<Vector2, Waypoint> entry : map.entrySet()) {
+			Waypoint waypoint = entry.getValue();
+			waypoint.exploredByLooking = null;
+			waypoint.explorerPos = null;
+			waypoint.isExplored = false;
+		}
+		
+		//Queue für noch zu erkundende Nachbarn
+		Queue<Waypoint> queue = new LinkedList<Waypoint>();
+		
+		//Speichern von Start, Ende und derzeitigen Waypoint
+		Waypoint start = map.get(startPos);
+		Waypoint end = map.get(targetPos);
+		Waypoint searchingFrom = null;
+		boolean isRunning = true;
+		
+		//Füge Start hinzu
+		System.err.println(start);
+		queue.add(start);
+
+        while (queue.size() > 0 && isRunning)
+        {
+            searchingFrom = queue.poll();
+            searchingFrom.isExplored = true;
+
+            if (searchingFrom.position.equals(end.position)) isRunning = false;
+            
+            //Explore Neighbours
+            if (!isRunning) break;
+            for (Direction direction : Direction.values())
+            {
+                Vector2 neighbourPos = Vector2.AddUp(searchingFrom.position, Vector2.directionToVector(direction));
+
+                if(map.containsKey(neighbourPos) && map.get(neighbourPos).waypointType != WaypointType.WALL)
+                {
+                	//Add neighbour to Queue
+                	Waypoint neighbour = map.get(neighbourPos);
+                    if (neighbour.isExplored || queue.contains(neighbour))
+                    {
+                        //do nothing
+                    }
+                    else
+                    {
+                        queue.add(neighbour);
+                        neighbour.explorerPos = searchingFrom.position;
+                        neighbour.exploredByLooking = direction;
+                        neighbour.isExplored = true;
+                    }
+                }
+            }
+        }
+        
+        //Create Path
+        wayToPoint = new ArrayList<Waypoint>();
+
+        Waypoint previous = end;
+        while (!previous.position.equals(start.position))
+        {
+            wayToPoint.add(previous);
+            previous = map.get(previous.explorerPos);
+        }
+        //wayToPoint.add(start);
+        
+        //Reverse List
+        Collections.reverse(wayToPoint);
 	}
 }
--- src/de/vitbund/vitmaze/players/Waypoint.java
+++ src/de/vitbund/vitmaze/players/Waypoint.java
@@ -14,6 +14,7 @@ public class Waypoint {
 	public Vector2 position;
 	public Vector2 explorerPos;
 	public Direction exploredByLooking;
+	public boolean isExplored = false;
 	
 	public Waypoint(WaypointType waypointType, Vector2 pos) {
 		this.waypointType = waypointType;
@@ -29,6 +30,34 @@ public class Waypoint {
 		case "FORM": return WaypointType.FORM;
 		default: return null;
 		}
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((position == null) ? 0 : position.hashCode());
+		result = prime * result + ((waypointType == null) ? 0 : waypointType.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Waypoint other = (Waypoint) obj;
+		if (position == null) {
+			if (other.position != null)
+				return false;
+		} else if (!position.equals(other.position))
+			return false;
+		if (waypointType != other.waypointType)
+			return false;
+		return true;
 	}
 
 }
